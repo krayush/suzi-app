cb9fad44c63c166709ed7b41034398e0ff33c4d3
Ayush Walia <krjean@gmail.com>
2024-12-26 21:24:11 +0530

    Adding view for fetching posts on demand

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,62 @@
-import { Text, View } from "react-native";
+import { useCallback, useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
+import { Button, Headline } from "react-native-paper";
+
+type Post = {
+  id: number;
+  title: string;
+};
 
 export default function Index() {
+  const [isFetching, setIsFetching] = useState(false);
+  const [posts, setPosts] = useState<Post[] | null>(null);
+
+  const fetchPosts = useCallback(() => {
+    setIsFetching(true);
+    try {
+      (async () => {
+        const res = await fetch("https://dummyjson.com/posts");
+        const data = await res.json();
+        setPosts(data.posts);
+      })();
+    } catch {
+    } finally {
+      setTimeout(() => {
+        setIsFetching(false);
+      }, 200);
+    }
+  }, [setIsFetching, setPosts]);
+
+  const clearPosts = useCallback(() => setPosts([]), []);
+
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        padding: 8,
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View style={{ flexDirection: "row", gap: 8 }}>
+        <Button onPress={fetchPosts}>Fetch</Button>
+        <Button onPress={clearPosts}>Clear</Button>
+      </View>
+      {isFetching && <ActivityIndicator />}
+      {posts && (
+        <FlatList
+          data={posts}
+          keyExtractor={(item) => String(item.id)}
+          style={{
+            padding: 8,
+          }}
+          renderItem={({ item }) => (
+            <View style={{ minHeight: 30 }}>
+              {/* <Button> */}
+              <Text>{item.title}</Text>
+              {/* </Button> */}
+            </View>
+          )}
+        />
+      )}
     </View>
   );
 }
